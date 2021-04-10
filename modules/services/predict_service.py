c860fb1ea77c30640de4f303f21a269da16d6912
Vinayaks117 <37987800+Vinayaks117@users.noreply.github.com>
2021-04-10 18:57:39 +0530

    Updated predict_service.py file

--- modules/services/predict_service.py
+++ modules/services/predict_service.py
@@ -7,23 +7,15 @@ class PredictService:
 
     @classmethod
     def predict_accidentLevel(cls, predict_request):
-        cwd = os.getcwd()
-        print('cwd:',cwd)
-        #filelocation = (cwd + '/modules/saved_models/')
-        #print('filelocation',filelocation)
         
+        # Set current working directory
+        cwd = os.getcwd()
         if cwd == '/app':
             os.chdir(cwd + '/modules/saved_models')
-            
-        print('cwd:',os.getcwd())
-        #os.chdir('C:/Users/Prakash/PycharmProjects/AutoMLCapstoneProject/modules/saved_models')
-        #filelocation = (cwd + '/PycharmProjects/AutoMLCapstoneProject/modules/saved_models/')  # For Linux
-        #filename = (filelocation + 'finalised_ann_model.h5')
+   
         filename = ('finalised_ann_model.h5')
-        print('filename',filename)
 
         # Load the model
-        #model = pickle.load(open('finalised_ml_model.sav','rb'))
         model = load_model(filename)
 
         accidentLevel = (np.asarray(model.predict(predict_request))).round()
