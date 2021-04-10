98645a8d07fc635bd0150214d84c0d6b1fdbec4e
Vinayaks117 <37987800+Vinayaks117@users.noreply.github.com>
2021-04-10 17:57:50 +0530

    Updated predict_service.py file

--- modules/services/predict_service.py
+++ modules/services/predict_service.py
@@ -8,11 +8,14 @@ class PredictService:
     @classmethod
     def predict_accidentLevel(cls, predict_request):
         cwd = os.getcwd()
+        print('cwd:',cwd)
         filelocation = (cwd + '/modules/saved_models/')
+        print('filelocation',filelocation)
 
         #os.chdir('C:/Users/Prakash/PycharmProjects/AutoMLCapstoneProject/modules/saved_models')
         #filelocation = (cwd + '/PycharmProjects/AutoMLCapstoneProject/modules/saved_models/')  # For Linux
         filename = (filelocation + 'finalised_ann_model.h5')
+        print('filename',filename)
 
         # Load the model
         #model = pickle.load(open('finalised_ml_model.sav','rb'))
