2a98ea55dac02f3dd5aa1f2e26554e40f8368778
Vinayaks117 <37987800+Vinayaks117@users.noreply.github.com>
2021-04-10 18:43:07 +0530

    Updated predict_service.py file

--- modules/services/predict_service.py
+++ modules/services/predict_service.py
@@ -9,10 +9,10 @@ class PredictService:
     def predict_accidentLevel(cls, predict_request):
         cwd = os.getcwd()
         print('cwd:',cwd)
-        filelocation = (cwd + '/modules/saved_models/')
+        #filelocation = (cwd + '/modules/saved_models/')
         print('filelocation',filelocation)
         
-        os.chdir(cwd + '/modules/saved_models')
+        #os.chdir(cwd + '/modules/saved_models')
         print('cwd:',os.getcwd())
         #os.chdir('C:/Users/Prakash/PycharmProjects/AutoMLCapstoneProject/modules/saved_models')
         #filelocation = (cwd + '/PycharmProjects/AutoMLCapstoneProject/modules/saved_models/')  # For Linux
